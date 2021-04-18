aafe2f1230240d540bb285782b08fb2a6f9c8fd7
Shravan Kumar <kshravan694@gmail.com>
2021-04-18 19:25:21 +0530

    added initialization func and place class

--- simulation.py
+++ simulation.py
@@ -1,7 +1,12 @@
 import random
+places = []
+blobs = []
+
+
+
 
 class Blob:
-    def __init__ (self, age, productivity, locationx= (0,0)):
+    def __init__ (self, age, productivity, location= [0,0]):
         self.age = age
         self.productivity = productivity
         self.location = location
@@ -10,10 +15,23 @@ class Blob:
     def checkreproducing(self):
         if self.nourishment >= 8:
             self.isReproducing = 1
+    def feed(self):
+        for place in places:
+            if place.location == self.location:
+                if place.food > 0 and self.nourishment<10:
+                    place.food -= 1
+                    self.nourishment = 10
         
 
+class Place:
+    def __init__ (self, food, location):    
+        self.location = location
+        self.food = food
+
+def initialize(foodmax, blobs, totalplaces):
+    for x in range(totalplaces[0]):
+        for y in range(totalplaces[1]):
+            places.append(Place(random.randint(0, int(foodmax)), (int(x), int(y))))
+            y+=1
+        x+=1
     
-
-
-
-
